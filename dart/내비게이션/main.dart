a7dd7ed04ccdaf262103a553d7368a3b57d58876
ASD78935 <zzz869@g.shingu.ac.kr>
2025-04-15 15:44:11 +0900

    Add files via upload

---
+++ dart/내비게이션/main.dart
@@ -0,0 +1,28 @@
+import 'package:a3/FirstPage.dart';
+import 'package:flutter/material.dart';
+
+import 'SecondPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: FirstPage(),
+      routes: {
+        '/first': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+      },
+    );
+  }
+}
